a45217b3d7baf856d4a190dc68d6fd7c91247825
SmileeCj <chejaiismail@gmail.com>
2023-06-16 17:46:12 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - function to start the program
+ *
+ * Description: print the alphabet
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (0 <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf('\n');
+
+	return (0);
+}
