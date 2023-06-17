30da116c11f0036501056edc82fcaa13424867c9
SmileeCj <chejaiismail@gmail.com>
2023-06-16 17:03:07 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - function to start the program
+ *
+ * Description: print the alphabet
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char cha = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(cha);
+		cha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
