930edf6b1a6a6c25a4b4c84642edb0cd7769bc05
SmileeCj <chejaiismail@gmail.com>
2023-06-16 18:00:42 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - function to start the program
+ *
+ * Description: print numbers and convert them to char
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num)
+		num++;
+	}
+	putchar("\n");
+
+	return (0);
+}
