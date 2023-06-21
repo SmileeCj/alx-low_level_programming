25990ff28f6ed6f085495aa6a71262ae9105c643
SmileeCj <chejaiismail@gmail.com>
2023-06-21 16:33:53 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _abs - function to start the program
+ *
+ * Description: function that returns an absolute number
+ *
+ * @n: function parameter
+ *
+ * Return: returns n
+*/
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		n *= (-1);
+	}
+	return (n)
+	_putchar('\n');
+}
