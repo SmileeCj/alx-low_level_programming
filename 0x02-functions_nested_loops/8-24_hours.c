124611d0a7ef0baebc044efe86131f5597a087fc
SmileeCj <chejaiismail@gmail.com>
2023-06-21 17:20:27 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j <= 59; j++)
 		{
 			_putchar((i / 10) + 48);
 			_putchar((i % 10) + 48);
@@ -25,5 +25,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
