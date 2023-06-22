c09688b1d288cd2f8a3466b00a79721a8e671f1b
SmileeCj <chejaiismail@gmail.com>
2023-06-21 17:55:40 -0700

    task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * times_table - function to start the program
+ *
+ * Description: prints tables of nine
+ *
+*/
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+
+			_putchar(44);
+			_putchar(32);
+
+			if (res <= 9)
+			{
+				_putchar((32);
+			}
+			else
+			{
+				_putchar((res / 10) + 48);
+			}
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
