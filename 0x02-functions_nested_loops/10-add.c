704f7b3a55f4b6ec19292099a73085d6d72fe28f
SmileeCj <chejaiismail@gmail.com>
2023-06-21 18:27:12 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * add - function to start the program
+ *
+ * Description: sum of two inputs
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+
+	int res = a + b;
+
+	return (res);
+
+}
