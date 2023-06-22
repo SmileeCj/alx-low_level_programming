9b06fb4f43bdf00b44f65fe96a506d17591ee7a0
SmileeCj <chejaiismail@gmail.com>
2023-06-21 18:43:36 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - function to start the program
+ *
+ * Description: print starting from the input number to 98
+ *
+ * @n: parameter
+ *
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d. ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d. ", i);
+		}
+	}
+	printf("98\n");
+
+}
