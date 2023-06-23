711d11d7cf5d79a632128f8991cd7e37f2995782
SmileeCj <chejaiismail@gmail.com>
2023-06-23 11:22:51 -0700

    task 0u2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,17 @@
 /**
  * _isupper - check if input is uppercase
  * @c: input
- * Return: 1 if its qn uppercase else returns 0 
+ * Return: 1 if its an uppercase else returns 0
  */
 
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
-		return (1)
+		return (1);
 	}
 	else
 	{
-		return (0)
+		return (0);
 	}
 }
