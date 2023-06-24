1191938f3005d50b2d61c5097d9207fc9ea47aab
SmileeCj <chejaiismail@gmail.com>
2023-06-23 19:33:07 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ *
+ * Return: 0 success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
