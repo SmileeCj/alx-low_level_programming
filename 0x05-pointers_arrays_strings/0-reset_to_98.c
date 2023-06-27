36e8b6f6ae3e6e92a1cef0d0ff7c34f4dc97ed13
SmileeCj <chejaiismail@gmail.com>
2023-06-26 18:06:35 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value that it point to with the input
+ * @a: function parameter
+ * Return: Nothing
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
