52e82d3a1a2f52e938df233e06e238adbf24d431
SmileeCj <chejaiismail@gmail.com>
2023-06-26 19:17:31 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print a reversed string
+ * @s: function parameter
+ * Return: nothing
+ */
+
+oid print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i])
+	{
+		i++;
+	}
+	for (j = i; j > 0; j--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
