247e3507629a9bc482a4c1ca5c7af595968362d5
SmileeCj <chejaiismail@gmail.com>
2023-06-30 19:44:38 -0700

    task 78

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - turn letters to 1337
+ * @c: function parameter one
+ * Return: encoded letters
+ */
+
+char *leet(char *c)
+{
+	int *a = c;
+	char k[] = {'A', 'E', 'O', 'T', 'L'};
+	int num[] = {4, 3, 0, 7, 1};
+	int i;
+	int size = sizeof(k);
+
+	while (*c)
+	{
+		for (i = 0; i < size / sizeof(char); i++)
+		{
+			if (*c == k[i] || *c == k[i] + 32)
+			{
+				*c = 49 + num[i];
+			}
+		}
+		return (c++);
+	}
+	return (a);
+}
