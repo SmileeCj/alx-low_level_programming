017c665f2180b21c13ed76824693b55a51357100
SmileeCj <chejaiismail@gmail.com>
2023-07-05 18:12:00 -0700

    tasks

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a reversed string
+ * @s: parameter
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
+	else
+	{
+		_putchar('\n');
+	}
+}
