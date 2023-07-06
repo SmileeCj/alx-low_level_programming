9cc70dc4d0457937a550ac1903a6bf101285ae7b
SmileeCj <chejaiismail@gmail.com>
2023-07-05 19:55:02 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"i
+
+int sqr(int n, int i);
+
+/**
+ * _sqrt_recursion - function calculate power of an number
+ * @n: parameter1
+ * Return: square root
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
+
+
+/**
+ * sqr - function to find square root
+ * @i: parameter2
+ * @n: param
+ * Return: int number
+ */
+
+
+int sqr(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i < n)
+	{
+		return (sr(n, i + 1));
+	}
+}
