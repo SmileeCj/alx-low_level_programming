1d4292a87519241c86117410d1f24d8ff1e6222f
SmileeCj <chejaiismail@gmail.com>
2023-07-08 02:49:24 +0100

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: int
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int j = 0;
+	
+	if (argc < 3)
+	{
+		printf("there is an Error\n");
+		return (0);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		
+		printf("%d\n", i * j);
+	}
+
+}
