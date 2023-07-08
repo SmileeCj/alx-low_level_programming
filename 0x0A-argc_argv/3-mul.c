1ecf3bb8bcc06819ea0dd1325c18da80bed47e23
SmileeCj <chejaiismail@gmail.com>
2023-07-08 03:12:13 +0100

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,16 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int j = 0;
 
-	if (argc == 3)
+	if (argc < 3)
 	{	
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-
-		printf("%d\n", i * j);
-	}
-	else
-	{
 		printf("there is an Error\n");
 		return (1);
+	}
+	else
+	{I	
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		
+		printf("%d\n", (i * j));
 	}
 }
