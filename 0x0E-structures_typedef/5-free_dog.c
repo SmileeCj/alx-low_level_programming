fcd5290140bd9b07ab24150a5dd8574aee261ca5
SmileeCj <chejaiismail@gmail.com>
2023-07-17 16:37:38 +0100

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,12 +6,19 @@
  * @d: struct dog to free
  * Return: void
  */
+
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->owner);
-		free(d->name);
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		if (d->name)
+		{
+			free(d->name);
+		}
 		free(d);
 	}
 }
