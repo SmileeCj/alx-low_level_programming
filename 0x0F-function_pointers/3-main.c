8794daa4da83d2ca3f61acb99e908ec1cea11113
SmileeCj <chejaiismail@gmail.com>
2023-07-22 03:25:09 +0100

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int a, b;
+	char *op_funct;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	op_funct = argv[2];
+
+	if (get_op_func(op_funct) == NULL || op_funct[1] != '\0')
+		printf("Error\n"), exit(99);
+
+	if ((*op_funct == '/' && b == 0) || (*op_funct == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op_funct)(a, b));
+
+	return (0);
+}
