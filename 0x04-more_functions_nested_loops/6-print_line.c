c3e065e3c01574670d69f4ed36982572cb5b736b
SmileeCj <chejaiismail@gmail.com>
2024-01-28 17:20:56 +0100

    add a closing bracket

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,16 @@
  */
 
 void print_line(int n)
+{
+	if (n <= 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	} else
+	{
+		for (int i = 1; i <= n; i++)
 		{
-			_putchar('\n');
-		} else
-		{
-			for (int i = 1; i <= n; i++)
-			{
-				_putchar('-');
-			}
-			_putchar('\n');
+			_putchar('-');
 		}
+		_putchar('\n');
 	}
+}
