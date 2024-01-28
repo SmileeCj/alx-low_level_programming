27a89a6574af7443b681fa4969b425ea9ffcc02b
SmileeCj <chejaiismail@gmail.com>
2024-01-28 17:05:26 +0100

    exercise 6 draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *	print_line - func draws a straight line in the terminal
+ *	@n: is an int parameter
+ *	Return:nothing
+ */
+
+void print_line(int n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		} else
+		{
+			for (int i = 1; i <= n; i++)
+			{
+				_putchar('-');
+			}
+			_putchar('\n');
+		}
