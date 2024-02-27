5b2587201415c8190ca48b18b579fb3dc3bb13c1
SmileeCj <chejaiismail@gmail.com>
2024-02-27 04:07:34 -0800

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a revesed string, followed by a new line
+ * @s : pointer to string
+ * return : nothing
+ */
+
+void _print_rev_recursion(char *s);
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(&s[i] + 1);
+	}
+	putchar(s[i]);
+}
