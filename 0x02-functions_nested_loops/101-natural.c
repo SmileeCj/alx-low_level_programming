67be0ea3622df2f71324fc0a29d8ecd65f9a1402
SmileeCj <chejaiismail@gmail.com>
2024-03-05 07:17:53 -0800

    101 task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print the sum of multiples
+ * Return: Always 0 (success)
+*/
+
+
+int main(void)
+{
+	int i = 0, sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			sum += i;
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+		i++;
+	}
+	printf("%d", sum);
+	return (0);
+}
