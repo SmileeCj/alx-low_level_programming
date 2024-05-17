c404dc3c44c964409054904937bb24525b55c6fe
SmileeCj <chejaiismail@gmail.com>
2024-05-17 19:43:07 +0100

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strdup - duplicate a string
+* @str: char pointer
+* Description: creates a duplicate sring,
+* Return: a pointer
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int size = 0;
+	int i = 0;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+
+	p = malloc((size + 1) * sizeof(char));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = str[i];
+	}
+	p[size] = '\0';
+	return (p);
+}
