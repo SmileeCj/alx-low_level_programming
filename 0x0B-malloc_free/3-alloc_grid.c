7480df53ec10b093de89b65b06c0f994350eb623
SmileeCj <chejaiismail@gmail.com>
2024-05-18 16:43:23 +0100

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* alloc_grid - concat two strings
+* @width: int
+* @height: int
+* Description: creates a 2D array
+* Return: a pointer
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **p;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	p = (int **)malloc(sizeof(int* ) * height);
+
+	for (i = 0; i < height; i++)
+		p[i] = (int* )malloc(sizeof(int) * width);
+
+	return (p);
+}
