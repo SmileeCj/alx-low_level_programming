cf83620510605e2383f0a5ab0b97ace1459024aa
SmileeCj <chejaiismail@gmail.com>
2024-05-29 21:40:00 +0100

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,14 +12,14 @@
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *p;
+	int *p;
 
-	p = malloc(sizeof(unsigned int) * b);
+	p = malloc(b);
 
 	if (p == NULL)
 	{
 		exit(98);
 	}
-
+	
 	return (p);
 }
