1be53d59934e993686c26c1a100683f201449ea0
SmileeCj <chejaiismail@gmail.com>
2024-06-25 13:41:11 +0100

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+/**
+* main - is entry function
+* @argc: int
+* @argv: array
+* Description: print file name
+* Return: 0 (success)
+*/
+
+
+int main()
+{
+		printf("%s\n",__FILE__);
+		return (0);
+
+}
+
