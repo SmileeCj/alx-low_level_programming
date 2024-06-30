c76270a73397dd2eccfdbea3b5d692c0e0e771a8
SmileeCj <chejaiismail@gmail.com>
2024-06-30 13:56:33 +0100

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  *
  * Description: define new type called dog
  */
-
 typedef struct dog
 {
 	char *name;
@@ -17,7 +16,8 @@ typedef struct dog
 	char *owner;
 } dog;
 
-#define dog_t dog
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
