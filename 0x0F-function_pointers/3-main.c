420844d96bf0928cbf75206e04498939aa69cc1d
SmileeCj <chejaiismail@gmail.com>
2024-07-14 23:14:53 +0100

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry function
+* @argc: param1
+* @argv: param2
+*
+* Description: main function
+*
+* Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	int (*op_fun)(int, int);
+	char *p_sign = &argv[2][0];
+	int num1 = 0, num2 = 0;
+
+	if (argc != 4)
+	{
+		printf("Error98\n");
+		exit(98);
+	}
+
+	op_fun = get_op_func(argv[2]);
+	if (!op_fun)
+	{
+		printf("Error98\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((*p_sign == '/' || *p_sign == '%') && !num2)
+	{
+		printf("Error100\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_fun(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include "3-calc.h"
+
+/**
+* op_add - adding two numbers
+* @a: param1
+* @b: param2
+*
+* Description: adding numbers
+*
+* Return: int
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two numbers
+ * @a: param1
+ * @b: param2
+ *
+ * Description: sub numbers
+ *
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two numbers
+ * @a: param1
+ * @b: param2
+ *
+ * Description: product of numbers
+ *
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide numbers
+ *
+ * @a: param1
+ * @b: param2
+ *
+ * Description: divide two numbers
+ *
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of number
+ *
+ * @a: param1
+ * @b: param2
+ *
+ * Description: reminder of a number
+ *
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
