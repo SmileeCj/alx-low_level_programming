08499d81fef1e0d7c7105f02954c6541f92c28a2
SmileeCj <chejaiismail@gmail.com>
2024-08-14 14:57:53 +0100

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - list length
+ *
+ * @h: Head
+ *
+ * Description: list length
+ *
+ * Return: size_t
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+	if (h)
+	{
+		size_t i = 0;
+		listint_t *ptr = h;
+
+		while (ptr != NULL)
+		{
+			i++;
+			ptr = ptr->next;
+		}
+
+		return (i);
+	}
+}
