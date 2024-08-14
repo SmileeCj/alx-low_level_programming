98be82f3ed25aeaf08f22a82b62b88b7f0215330
SmileeCj <chejaiismail@gmail.com>
2024-08-14 15:20:47 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,13 +16,12 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 	const listint_t *ptr = h;
 
-
-		while (ptr != NULL)
-		{
-			i++;
-			printf("%d\n", ptr->n);
-			ptr = ptr->next;
-		}
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		i++;
+		ptr = ptr->next;
+	}
 	return (i);
 
 }
