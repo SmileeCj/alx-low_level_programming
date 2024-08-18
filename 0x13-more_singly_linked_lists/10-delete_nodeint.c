8759e6a673ad1d1f75caeddc9cb1c9a48ca0d590
SmileeCj <chejaiismail@gmail.com>
2024-08-18 02:04:15 +0100

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+
+
+/**
+ * list_len - length of list
+ * @head: Head
+ * Desription: length of the list
+ * Return: unsigned int
+ */
+
+unsigned int list_len(listint_t *head)
+{
+	unsigned int len = 0;
+	listint_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		len++;
+		ptr = ptr->next;
+	}
+
+	return (len);
+}
+
+
+/**
+ * delete_nodeint_at_index - delete in nth
+ *
+ * @head: Head
+ * @index: index
+ * Description: delete node at index
+ *
+ * Return: int
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int length = 0, i = 0;
+	listint_t *ptr = *head;
+	listint_t *prev = ptr;
+
+	if (head == NULL)
+		return (-1);
+
+	length = list_len(*head);
+
+	if (index == 0)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+		return (0);
+	}
+	while (ptr->next != NULL && (i < index && index < length))
+	{
+		if ((i > length && i != index))
+			return (-1);
+
+		prev = ptr;
+		ptr = ptr->next;
+		i++;
+	}
+
+	prev->next = ptr->next;
+	free(ptr);
+	ptr = NULL;
+
+	return (0);
+}
