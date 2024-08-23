f51f8b468e6f796e0907f0463beaa63b0d938b29
SmileeCj <chejaiismail@gmail.com>
2024-08-23 16:16:04 +0100

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - print a number of bytes
+ *
+ * @filename: param1
+ * @letters: param2
+ *
+ * Description: printing number of bytes
+ *
+ * Return: 0 (success)
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd = -1, count = 0, w_count = 0;
+	char BUFFER[BUF_S] = {0};
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	count = read(fd, BUFFER, letters);
+	w_count = write(1, BUFFER, count);
+
+	if (count == -1)
+		return (0);
+
+	close(fd);
+	return (w_count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUF_S 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+#endif
