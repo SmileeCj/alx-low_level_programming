d257fcae68aa2f52cf5b153dfd6c32d5f897eae1
SmileeCj <chejaiismail@gmail.com>
2024-08-25 03:29:21 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * create_file - creat a file
  *
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	w_count = write(fd, text_content, sizeof(text_content));
+	w_count = write(fd, text_content, strlen(text_content));
 
 	if (w_count == -1)
 	{
