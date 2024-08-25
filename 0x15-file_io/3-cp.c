32da509ab228edc9b35404a87c6f6a3c276a3210
SmileeCj <chejaiismail@gmail.com>
2024-08-25 15:27:46 +0100

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - entry function
+ *
+ * @argc: param1
+ * @argv: param2
+ *
+ * Description: copy from file to file
+ *
+ * Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	int fd_from = -1, fd_to = -1;
+	ssize_t w_count = 0, r_count = 0;
+	char buffer[BUF_S] = {0};
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, ERR_ARG), _exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, ERR_RD), _exit(98);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, ERR_WR), _exit(99);
+
+	while ((r_count = read(fd_from, buffer, BUF_S)))
+	{
+		w_count = write(fd_to, buffer, r_count);
+		if (w_count != r_count)
+			dprintf(STDERR_FILENO, ERR_WR), _exit(99);
+	}
+
+	if (w_count == -1)
+		dprintf(STDERR_FILENO, ERR_WR), _exit(99);
+
+	if ((close(fd_to)) == -1)
+		dprintf(STDERR_FILENO, ERR_CLOSE, fd_to), _exit(100);
+
+	if ((close(fd_from)) == -1)
+		dprintf(STDERR_FILENO, ERR_CLOSE, fd_from), _exit(100);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,12 @@
 
 #define BUF_S 1024
 
+#define ERR_ARG "Usage: cp file_from file_to\n"
+#define ERR_RD "Error: Can't read from file NAME_OF_THE_FILE\n"
+#define ERR_WR "Error: Can't write to NAME_OF_THE_FILE\n"
+#define ERR_CLOSE "Error: Can't close fd %d\n"
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
